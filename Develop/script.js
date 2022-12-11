97884ea6352e083539281aab01be87ea2966d508
Kristina Senters <kristina@Kristinas-iMac.lan>
2022-12-10 20:35:11 -0500

    logic for user property submission

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,11 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passLength;
+var isLowCase;
+var isUpCase;
+var isNum;
+var isSpecial;
 
 // Write password to the #password input
 function writePassword() {
@@ -13,17 +18,54 @@ function writePassword() {
 
 // Prompt asking user for their desired password length
 function askPasswordWordLength() {
-  window.prompt("Type the password length (8 - 128 char): ");
+  var isLongEnough = false;
+  while(!isLongEnough) {
+    var passLength = window.prompt("Type the password length (8 - 128 char): ");
+    if(passLength < 8 || passLength > 128) {
+      isLongEnough = false;
+    } else {
+      isLongEnough = true;
+    }
+  }
+  this.passLength = passLength;
 }
 
-// Prompt asking user for other desired properties of their password
-function askVariousProperties() {
-  window.prompt("Other thing");
+// Prompt asking user if allow lower case
+function askLowerCase() {
+  let isLowCase = confirm("Do You Want To Allow Lower Case Letters?");
+  this.isLowCase = isLowCase;
+}
+
+function askUpperCase() {
+  let isUpCase = confirm("Do You Want To Allow Upper Case Letters?");
+  this.isUpCase = isUpCase;
+}
+
+function askNumeric() {
+  let isNum = confirm("Do You Want To Allow Numbers?");
+  this.isNum = isNum;
+}
+
+function askSpecial() {
+  let isSpecial = confirm("Do You Want To Allow Special Characters");
+  this.isSpecial = isSpecial;
 }
 
 // Generates and displays password
 function generatePassword() {
-
+  var count = 0;
+  if(this.isLowCase) {
+    count++;
+  }
+  if(this.isUpCase) {
+    count++;
+  }
+  if(this.isNum) {
+    count++;
+  }
+  if(this.isSpecial) {
+    count++;
+  }
 }
 
 // Add event listener to generate button
